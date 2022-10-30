a9bbc40d595d7d30fc158918799fd5f9d1d1d7cb
sipara <sipara@DESKTOP-333ECF9>
2022-10-30 19:18:07 +0100

    Add project files.

---
+++ CodeWar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] greeting = { "edu", "sis", "robi" };
+            foreach(string i in greeting)
+            {
+                Console.WriteLine("hallo how you guys doing {0}", i);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
